d5696f8a7569158c22f6a476ba5e5e49f6d84189
DEBUG <demoorbug@gmail.com>
2022-07-07 12:27:49 +0800

    flexipatch

--- backup.sh
+++ backup.sh
@@ -4,18 +4,23 @@ rm -rf ./arch/dwm
 rm -rf ./arch/st
 rm -rf ./arch/slstatus
 rm -rf ./arch/dmenu
+rm -rf ./arch/slock
 rm -rf ./arch/rofi
 rm -rf ./arch/.local/bin/script
+
 mkdir ./arch/dwm
 mkdir ./arch/st
 mkdir ./arch/slstatus
 mkdir ./arch/dmenu
 mkdir ./arch/rofi
+mkdir ./arch/slock
 mkdir -p ./arch/.local/bin/script
+
 cp -r $HOME/.config/dwm/* ./arch/dwm
 cp -r $HOME/.config/st/* ./arch/st
 cp -r $HOME/.config/slstatus/* ./arch/slstatus
-cp -r $HOME/.config/dmenu/* ./arch/dmenu
+cp -r $HOME/.config/dmenu-flexipatch/* ./arch/dmenu
+cp -r $HOME/.config/slock-flexipatch/* ./arch/slock
 cp -r $HOME/.config/rofi/* ./arch/rofi
 # 要使用这个电源插件还必须安装xorg-xinput, dunst, polkit 这三个都可以在pacman 安装
 cp -r $HOME/.local/bin/script/* ./arch/.local/bin/script
