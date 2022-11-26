13af4c2f5007601d3473b6f2a6adb0449ae0bc34
DEBUG <demoorbug@gmail.com>
2022-11-26 23:34:11 +0800

    auto update

--- fish/config.fish
+++ fish/config.fish
@@ -4,16 +4,24 @@ set fish_greeting ""
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+function nvm
+    bass source (brew --prefix nvm)/nvm.sh --no-us ';' nvm $argv
+end
 #aliases
 alias ll "exa -l -g --icons"
 alias ls "exa --icons"
 alias lst "exa --icons -T -L"
 alias lla "exa -l -g -a --icons"
-alias adblink "scrcpy -Sw --shortcut-mod=rsuper & VLC=/Applications/VLC.app/Contents/MacOS/VLC sndcpy"
+alias adblink "scrcpy -Sw --shortcut-mod=rsuper,lctrl+rsuper & VLC=/Applications/VLC.app/Contents/MacOS/VLC sndcpy "
+alias lg "lazygit"
+alias ghs "ghs.bash"
+alias y "y.bash"
 
-
+set -x NVM_DIR ~/.nvm
+nvm use default --silent
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /Users/demoorbug/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# eval /Users/demoorbug/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
