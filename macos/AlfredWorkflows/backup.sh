63d93d3d8a7bd9dc595cce4b7b045606f8a178ae
DEBUG <demoorbug@gmail.com>
2023-01-20 12:43:42 +0800

    auto update

--- macos/AlfredWorkflows/backup.sh
+++ macos/AlfredWorkflows/backup.sh
@@ -15,6 +15,7 @@ cp "$config"../.gitconfig "$mymacos"
 cp -r "$path" "$mymacos"AlfredWorkflows
 cp -r "$config"fish/ "$mymacos"fish
 cp -r "$config"../.local/user_bin/ "$mymacos"user_bin
+cp -r "$config"peco/ "$mymacos"peco
 
 git add .
 git ci -m 'auto update'
