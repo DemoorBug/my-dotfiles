8fddf9e350a17e19aa202b0773420915c0910595
DEBUG <demoorbug@gmail.com>
2023-01-20 13:04:00 +0800

    auto update

--- macos/backup.sh
+++ macos/backup.sh
@@ -12,7 +12,9 @@ if [ "$1" = "del" ]; then
 fi
 
 cp "$config"../.gitconfig "$mymacos"
-cp -r "$path" "$mymacos"AlfredWorkflows
+#cp -r "$path" "$mymacos"AlfredWorkflows
+rsync -av --exclude "node_modules" "$path" "$mymacos"AlfredWorkflows
+
 cp -r "$config"fish/ "$mymacos"fish
 cp -r "$config"../.local/user_bin/ "$mymacos"user_bin
 cp -r "$config"peco/ "$mymacos"peco
